88b0a550d7f8e78fffa9eb935609d6485b94c0a7
Panu Leppäniemi <me@panuleppaniemi.com>
2011-10-12 14:57:33 +0300

    Bugfix: factory references should be nullable.

--- tests/Xi/Doctrine/Fixtures/ReferenceTest.php
+++ tests/Xi/Doctrine/Fixtures/ReferenceTest.php
@@ -3,22 +3,37 @@ namespace Xi\Doctrine\Fixtures;
 
 class ReferenceTest extends TestCase
 {
+    public function setUp()
+    {
+        parent::setUp();
+        
+        $this->factory->defineEntity('SpaceShip');
+        $this->factory->defineEntity('Person', array(
+            'name' => 'Eve',
+            'spaceShip' => FieldDef::reference('SpaceShip')
+        ));
+    }
+    
     /**
      * @test
      */
     public function referencedObjectShouldBeCreatedAutomatically()
     {
-        $this->factory->defineEntity('SpaceShip');
-        $this->factory->defineEntity('Person', array(
-            'name' => 'John',
-            'spaceShip' => FieldDef::reference('SpaceShip')
-        ));
-        
         $ss1 = $this->factory->get('Person')->getSpaceShip();
         $ss2 = $this->factory->get('Person')->getSpaceShip();
         
         $this->assertNotNull($ss1);
         $this->assertNotNull($ss2);
         $this->assertNotSame($ss1, $ss2);
+    }
+    
+    /**
+     * @test
+     */
+    public function referencedObjectsShouldBeNullable()
+    {        
+        $person = $this->factory->get('Person', array('spaceShip' => null));
+        
+        $this->assertNull($person->getSpaceShip());
     }
 }
